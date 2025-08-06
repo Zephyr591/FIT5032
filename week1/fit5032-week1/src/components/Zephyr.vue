35f7e403a35b4de135cc15ffa14bfb1c602749ad
zephyrxu <sxuu0097@student.monash.edu>
2025-08-06 23:45:40 +1000

    Fix: add Vue project files instead of submodule

---
+++ week1/fit5032-week1/src/components/Zephyr.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <h1>Hi Zephyr Xu!</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  text-align: center;
+}
+</style>
